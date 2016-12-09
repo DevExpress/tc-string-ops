598e5631808c1a7e17bf4fc33b85ecf55817af9e
inikulin <ifaaan@gmail.com>
2016-12-09 17:31:44 +0300

    Implement tests

--- test/test.js
+++ test/test.js
@@ -3,15 +3,22 @@ import { Selector } from 'testcafe';
 fixture `Test string operations`
     .page `http://localhost:1337`;
 
-const form            = Selector('form');
-const textInput       = form.find('input[type=text]');
-const reverseBtn      = form.find('button').withText('Reverse string');
-const hashBtn         = form.find('button').withText('Hash');
-const resultContainer = Selector('#result');
+const result     = Selector('#result');
+const form       = Selector('form');
+const textInput  = form.find('input[type=text]');
+const reverseBtn = form.find('button').withText('Reverse string');
+const hashBtn    = form.find('button').withText('Hash');
 
 test('Reverse string', async t => {
     await t
         .typeText(textInput, 'Hi there!')
         .click(reverseBtn)
-        .expect(resultContainer.textContent).eql('!ereht iH');
+        .expect(result.textContent).eql('!ereht iH');
+});
+
+test('Get string hash', async t => {
+    await t
+        .typeText(textInput, 'Thanks for all the fish')
+        .click(hashBtn)
+        .expect(result.textContent).eql('cb22738ce963f968e30d58b7efc09e1c');
 });
